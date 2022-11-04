785da47a2f0ea389566ae2fd4f79678193cf4b22
ishmyles <33308871+ishmyles@users.noreply.github.com>
2022-11-04 07:35:20 +0000

    Added routing for /shorturl with passing tests

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require('node:dns');
 const cors = require('cors');
 const app = express();
+
+const mockDB = {};
+let currNum = 1;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,14 +13,41 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:id', function(req, res) {
+  res.redirect(mockDB[req.params.id]);
+});
+
+app.post('/api/shorturl', async (req, res) => {
+  const { url } = req.body;
+  const httpRegex = /(http(||s):\/\/)/g;
+  const urlString = (httpRegex.test(url)) 
+    ? new URL(url) : (!httpRegex.test(url)) ? new URL('http://' + url) : url;
+  
+  await dns.lookup(urlString.host, (err, add, fam) => {
+    if (err) return res.json({ error: 'invalid url' });
+
+    mockDB[currNum] = urlString.href;
+    currNum++;
+    console.log(mockDB)
+    res.json({ original_url : mockDB[currNum - 1], short_url : currNum - 1});
+  });
+
+  /* 
+  //Alternate solution just using REGEX
+  const httpRegex = /(http(||s):\/\/)/g;
+  
+  if (httpRegex.test(req.body.url) === false) return res.json({ error: 'invalid url' });
+
+    mockDB[currNum] = req.body.url
+    currNum++;
+    console.log(mockDB)
+    res.json({ original_url : mockDB[currNum - 1], short_url : currNum - 1});*/
 });
 
 app.listen(port, function() {
